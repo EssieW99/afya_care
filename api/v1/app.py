3c97ed025b94ec02d22f2efb05d2e50d4136f951
Esther Wanjiku <esther.wanjikum99@gmail.com>
2025-01-25 12:36:56 +0300

    Edit: Added a config file to declutter the file

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,19 +2,16 @@
 """ Flask application"""
 
 
+from api.v1.config import Config
 from api.v1.views import app_views
 from flask import Flask, jsonify, render_template, make_response
 from flask_session import Session
 from flask_cors import CORS
 import os
 
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
+app = Flask(__name__, template_folder=Config.TEMPLATE_FOLDER, static_folder=Config.STATIC_FOLDER)
+app.config.from_object(Config)
 app.register_blueprint(app_views)
-app.secret_key = 'deathly_hallows'
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.route('/')
